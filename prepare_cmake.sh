229f21915645a42e3e73cc5e4c11b19c19f95309
Matt Wilson <matthew.wilson.synesis@gmail.com>
2026-08-02 18:36:13 +1000

    chore: .sis abstraction of the helper scripts

--- prepare_cmake.sh
+++ prepare_cmake.sh
@@ -35,9 +35,8 @@ while [[ $# -gt 0 ]]; do
             ;;
         --help)
 
+            [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
             cat << EOF
-chomp is a small, standalone utility program that removes end-of-line sequence from lines in its input
-Copyright (c) 2020-2024, Matthew Wilson and Synesis Information Systems
 Creates/reinitialises the CMake build script(s)
 
 $ScriptPath [ ... flags/options ... ]
--- remove_cmake_artefacts.sh
+++ remove_cmake_artefacts.sh
@@ -31,9 +31,8 @@ while [[ $# -gt 0 ]]; do
     case $1 in
         --help)
 
+            [ -f "$Dir/.sis/script_info_lines.txt" ] && cat "$Dir/.sis/script_info_lines.txt"
             cat << EOF
-chomp is a small, standalone utility program that removes end-of-line sequence from lines in its input
-Copyright (c) 2020-2024, Matthew Wilson and Synesis Information Systems
 Removes all known CMake artefacts
 
 $ScriptPath [ ... flags/options ... ]
